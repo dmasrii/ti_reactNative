c2c4f057c7cdd42fae803b86f53e654f18162b08
dmasrii <dmasri@udesa.edu.ar>
2025-05-28 09:21:07 -0300

    Pequeños cambios

--- src/navigation/BottomTabs.js
+++ src/navigation/BottomTabs.js
@@ -4,7 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import {FontAwesome} from '@expo/vector-icons'
 import Home from '../screens/Home'
 import Profile from '../screens/Profile'
-import Post from '../components/Post'
+import NewPost from '../screens/NewPost'
 
 
 const Tab = createBottomTabNavigator();
@@ -17,8 +17,8 @@ export default function BottomTabs() {
         <Tab.Screen name="Profile"  component={Profile} options={{
             tabBarIcon: ()=> <FontAwesome name="user" size={24} color={'teal'} /> // me falta el de crear post 
         }}/>
-        <Tab.Screen  name="Posts"  component={Post} options={{
-            tabBarIcon: () => <FontAwesome name="paperclip" size={24} color={'firebrick'} />
+        <Tab.Screen  name="New-Post"  component={NewPost} options={{
+            tabBarIcon: () => <FontAwesome name="edit" size={24} color={'firebrick'} />
         }}/>
     </Tab.Navigator>
   )
